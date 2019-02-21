e6a34d1772aff3f4db44df3b590cdbeb48306132
filatov <filatov@axetta.ru>
2019-02-21 16:11:18 +0300

    Add Prototyp Application

---
+++ micro-prototyp-app/src/main/java/ru/iteco/micro/microprototypapp/controller/StartController.java
@@ -0,0 +1,20 @@
+package ru.iteco.micro.microprototypapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by FilatovNA(rogowasya@gmail.com) on 21.02.19.
+ */
+
+@RestController
+public class StartController {
+
+    @ResponseBody
+    @RequestMapping(value = "/")
+    public String home(){
+        return "<div style='color:green'>Prototyp application for MicroService!</div>";
+    }
+
+}
